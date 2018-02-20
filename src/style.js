3bfef0c068d7a1e159be15392e454f0afcc26dac
Akshat Garg <coolakshat24@gmail.com>
2018-02-20 12:43:39 +0530

    Add styles

---
+++ src/style.js
@@ -0,0 +1,27 @@
+// universal font style for whole app
+export { material as fontStyle } from "react-native-typography"
+
+// universal color style for whole app
+export const colorStyle = {
+  superDark: "#000000",
+  dark: "#222222",
+  semiDark: "#4f4f4f",
+  medium: "#828282",
+  semiLight: "#bdbdbd",
+  light: "#e0e0e0",
+  superLight: "#f2f2f2",
+  default: "#2bbbad",
+  defaultDark: "#00695c",
+  primary: "#33b5e5",
+  primaryDark: "#0099cc",
+  secondary: "#aa66cc",
+  secondaryDark: "#9933cc",
+  danger: "#ff4444",
+  dangerDark: "#cc0000",
+  warning: "#ffbb33",
+  warningDark: "#ff8800",
+  success: "#00c851",
+  successDark: "#007e33",
+  info: "#56ccf2",
+  infoDark: "#0d47a1",
+}
